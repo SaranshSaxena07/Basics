d7d9245a913c3357fc3934c9a6d79899751ecdfe
Saransh <1997saransh1997@gmail.com>
2022-08-13 02:08:25 +0530

    Second update version of hungry

--- hungry.py
+++ hungry.py
@@ -1,1 +1,7 @@
-print("I am hungry.")
+hungry = input("Are you hungry? (yes/no) \n")
+if hungry=="yes":
+    print("Have somthing to eat.")
+elif hungry=="no":
+    print("Focus on your work.")
+else:
+    print("Answer in yes or no only.")
