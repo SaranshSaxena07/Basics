c2be13a595b57936c3063a92ccd722726abc159e
Saransh <1997saransh1997@gmail.com>
2022-08-13 02:30:02 +0530

    Itallian

--- hungry.py
+++ hungry.py
@@ -1,6 +1,7 @@
 hungry = input("Are you hungry? (yes/no) \n")
 if hungry=="yes":
     print("Have somthing to eat.")
+    print("Have Itallian.")
 elif hungry=="no":
     print("Focus on your work.")
 else:
